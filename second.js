789991fa945e54c6361a08c8befcba20c66255ea
Sachin Rathod <sr5631080@gmail.com>
2025-08-23 15:54:42 +0530

    dsa

--- second.js
+++ second.js
@@ -5,5 +5,22 @@ function PrintHelloAgain() {
     console.log("Hello Again, World!");
 }
 
-PrintHello();
-PrintHelloAgain();
+
+function analixeComplexity() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < i; j++) {
+        // Some complex operation
+            console.log(i, " ", j);
+        }
+    }
+}
+
+function f1(n){
+    for (let i = 1; i < n; i *= 2) {
+        console.log(i);
+    }
+}
+f1(400);
+// analixeComplexity();
+// PrintHello();
+// PrintHelloAgain();
