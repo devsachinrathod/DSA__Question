c3057844b668ce45e9d16d84690df98490930692
Sachin Rathod <sr5631080@gmail.com>
2025-09-24 18:42:06 +0530

    recurrion

--- Recurrions/recurrion.js
+++ Recurrions/recurrion.js
@@ -1,18 +1,25 @@
-// https://github.com/devsachinrathod/DSA__Question.git
+// // https://github.com/devsachinrathod/DSA__Question.git
 
-//Q-1] Searching the highest valuse of an array.
+// //Q-1] Searching the highest valuse of an array.
 
-function realBackTracking(foots, index = 0, calculateHighestStep = []) {
-    // base case: when we reach the end
-    if (index === foots.length) {
-        return calculateHighestStep;
-    }
-    // check and push
-    if (foots[index] > calculateHighestStep[0] || calculateHighestStep.length === 0) {
-        calculateHighestStep[0] = foots[index];
-    }
-    // recursive call
-    return realBackTracking(foots, index + 1, calculateHighestStep);
+// function realBackTracking(foots, index = 0, calculateHighestStep = []) {
+//     // base case: when we reach the end
+//     if (index === foots.length) {
+//         return calculateHighestStep;
+//     }
+//     // check and push
+//     if (foots[index] > calculateHighestStep[0] || calculateHighestStep.length === 0) {
+//         calculateHighestStep[0] = foots[index];
+//     }
+//     // recursive call
+//     return realBackTracking(foots, index + 1, calculateHighestStep);
+// }
+// let step = [21, 2, 4, 6, 8, 67];
+// console.log("Highest Step:", realBackTracking(step)[0]);
+function reverseString(str) {
+  if (str === "") return "";      // base case
+  return reverseString(str.slice(1)) + str[0];
 }
-let step = [21, 2, 4, 6, 8, 67];
-console.log("Highest Step:", realBackTracking(step)[0]);
+
+console.log(reverseString("Hiithere")); // "erehtiiH"
+
