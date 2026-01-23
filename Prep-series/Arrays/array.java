c4455532d3748bc02241f2e0ce02f7c5105e38f7
Sachin Rathod <sr5631080@gmail.com>
2026-01-23 20:50:41 +0530

    the add fimction of displaying thr duplicate value of array

--- Prep-series/Arrays/array.java
+++ Prep-series/Arrays/array.java
@@ -1,25 +1,60 @@
+class ArrayStore {
+    int[] arr = new int[10]; // storage
+    int index = 0;           // tracks position
 
-class array{
-  int val;
+    void insert(int val) {
+        arr[index] = val;
+        index++;
+    }
 
- array(int val){
-   this.val = val;
-  }
-  int insert(int val){
-    this.val = val;
-    return this.val;
-  }
-int getVal(){
-   return val;
-}
+    void printAll() {
+        for (int i = 0; i < index; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 
+  void binarySearch(int target) {
+        int left = 0;
+        int right = index - 1;
 
-  public static void main(String args[]){
-    array obj = new array();
-    obj.insert(10);
-    obj.insert(15);
-    System.out.println("Value: " + obj.getVal());
-    int[] arr = {obj.getVal()};
-    System.out.println("First element: " + arr[0]);
-  }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Found at index: " + mid);
+                return;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+     public boolean displayTheDuplicateValue(){
+        for(int i=0;i<index;i++){
+            for(int j=i+1;j<index;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("Duplicate value is: "+arr[i]);
+                    return true;
+                }
+            }
+        }
+        return false;
+    
+    }
+
+    public static void main(String[] args) {
+        ArrayStore obj = new ArrayStore();
+
+        obj.insert(12);
+        obj.insert(10);
+        obj.insert(15);
+        obj.insert(10); // duplicate
+        obj.insert(20);
+        obj.displayTheDuplicateValue();
+
+        obj.printAll();
+    }
 }
